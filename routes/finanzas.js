428d6bbfc62e8978e3d5a9dffe4cabe922d1c0ff
Mike <69970540+mikerb95@users.noreply.github.com>
2026-07-09 12:18:49 -0500

    feat: update periodoInv calculation to use invIncome for invoice subtotals

--- routes/finanzas.js
+++ routes/finanzas.js
@@ -130,7 +130,7 @@ router.get('/', requireAuth, requireAdmin, async (req, res) => {
   const totalIngresosAllTime = paidInvoices.reduce((s, i) => s + invIncome(i), 0) + paidOrders.reduce((s, o) => s + o.total, 0);
   const totalEgresosAllTime  = gastos.reduce((s, g) => s + g.amount, 0);
 
-  const periodoInv  = paidInvoices.filter(i => inPeriod(invIncomeDate(i), year, month)).reduce((s, i) => s + i.subtotal, 0);
+  const periodoInv  = paidInvoices.filter(i => inPeriod(invIncomeDate(i), year, month)).reduce((s, i) => s + invIncome(i), 0);
   const periodoOrd  = paidOrders.filter(o => inPeriod(o.createdAt, year, month)).reduce((s, o) => s + o.total, 0);
   const periodoGas  = gastos.filter(g => inPeriod(g.date, year, month)).reduce((s, g) => s + g.amount, 0);
   const periodoIng  = periodoInv + periodoOrd;
